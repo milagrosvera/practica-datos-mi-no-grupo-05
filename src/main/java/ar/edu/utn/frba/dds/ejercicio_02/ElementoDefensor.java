1e534b3fdee9570a2a80e2a7d5844abfebb7599b
nachovilla123 <nacho.villarruel@gmail.com>
2024-09-11 15:26:02 -0300

    init + ejercicios

---
+++ src/main/java/ar/edu/utn/frba/dds/ejercicio_02/ElementoDefensor.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds.ejercicio_02;
+
+public interface ElementoDefensor {
+
+  public void defender(Personaje personaje);
+
+}
---
+++ src/main/java/ar/edu/utn/frba/dds/ejercicio_02/Escudo.java
@@ -0,0 +1,8 @@
+package ar.edu.utn.frba.dds.ejercicio_02;
+
+public class Escudo implements ElementoDefensor{
+  @Override
+  public void defender(Personaje personaje) {
+
+  }
+}
